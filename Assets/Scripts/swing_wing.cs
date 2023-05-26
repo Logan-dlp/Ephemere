ea2c7a7d696d763ebf0c3166f86d578b8a184c2a
Edgar Maurel <edgar.maurel@epitech.eu>
2023-05-27 01:49:34 +0200

    feat(gameplay): death trigger

--- Assets/Scripts/swing_wing.cs
+++ Assets/Scripts/swing_wing.cs
@@ -21,18 +21,19 @@ public class swing_wing : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (!wing.butterfly.isAlive)
+            return;
         if (down && angle < 0)
         {
-            wing.transform.RotateAround(wing.anchorPoint.position, wing.butterfly.pointingDir, 5 * wing.dir);
-            angle += 5;
+            wing.transform.RotateAround(wing.anchorPoint.position, wing.butterfly.pointingDir, 10 * wing.dir);
+            angle += 10;
         }
         if (!down && angle > -45)
         {
-            wing.transform.RotateAround(wing.anchorPoint.position, wing.butterfly.pointingDir, -5 * wing.dir);
-            angle -= 5;
-            
+            wing.transform.RotateAround(wing.anchorPoint.position, wing.butterfly.pointingDir, -10 * wing.dir);
+            angle -= 10;
         }
-        Vector3 force = new Vector3(wing.upwardForce * wing.dir, wing.upwardForce / 2, 0);
+        Vector3 force = new Vector3(wing.upwardForce * wing.dir, wing.upwardForce / 3, 0);
         if (Input.GetKeyDown(wing.keyCode))
         {
             down = true;
