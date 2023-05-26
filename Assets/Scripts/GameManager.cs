4e128dd0e9789cfb6c4ea59862a6e4e3a96d3ef5
Logan <pix.59700@gmail.com>
2023-05-26 21:18:04 +0200

    Display Time 🚧

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public float MaxTime = 100;
+    public Text TimeText;
 
+    public void DisplayTime(float _timer)
+    {
+        if (_timer <= 0)
+        {
+            _timer = 0;
+        }
+
+        float _hour = Mathf.FloorToInt(_timer / 60);
+        float _minute = Mathf.FloorToInt(_timer % 60);
+
+        TimeText.text = string.Format("{0:00}:{1:00}", _hour, _minute);
+    }
+    
     void Timer()
     {
         if (MaxTime >= 0)
         {
-            MaxTime -= Time.deltaTime;
+            MaxTime -= Time.deltaTime; // à changer pour pas que la partie dur 24min pcq Agathe veut pas :,(
         }
         else
         {
