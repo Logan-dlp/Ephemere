28e4839ce17495700d0af97abd70f1d869c518b7
Edgar Maurel <edgar.maurel@epitech.eu>
2023-05-27 07:06:05 +0200

    feat(gameplay): camera colliding with roof

---
+++ Assets/08 - Autre/Scripts/cameraBlock.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraBlock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.IgnoreLayerCollision(7, 7);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
